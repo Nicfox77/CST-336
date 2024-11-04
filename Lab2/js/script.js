c674387fe076e27982bbb48ef922b6eb02699b52
Nicholai Martinsen <nicfox77@gmail.com>
2024-11-04 00:52:57 -0800

    resets the guesses array when new game is initialized

--- Lab2/js/script.js
+++ Lab2/js/script.js
@@ -22,6 +22,7 @@ function initializeGame() {
     randomNumber = Math.floor(Math.random() * 99) + 1;
     console.log("randomNumber: " + randomNumber);
     attempts = 0;
+    guesses = [];
 
     //hiding the Reset button
     document.querySelector("#resetBtn").style.display = "none";
